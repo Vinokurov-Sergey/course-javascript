b52937bf8579ca1c1fa4aed1d966ec8a9ee8d254
Vinokurov-Sergey <lovekrishna@bk.ru>
2023-09-25 00:01:57 +0500

    week-2's tasks (project)

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -4,6 +4,22 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement (array) {
+    const count = array.length;
+    if (count === 0) {
+      return null;
+    }
+    const index = Math.floor(Math.random() * count);
+    return array[index];
+  },
+  getNextPhoto() {
+    // const friendsDB = require('./friends.json');
+    // const photosDB = require('./photos.json');
+    const friend = this.getRandomElement(friendsDB);
+    const name = friend.firstName;
+    const photo = this.getRandomElement(photosDB[friend.id]);
+    const url = photo.url;
+
+    return {name, url};
+  },
 };
